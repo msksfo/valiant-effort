2abc718138db4d7930140443ff896635df78deb9
Tia Esguerra <msksfo@gmail.com>
2019-06-28 08:55:17 -0700

    Feat: add code and styles for landing page mobile menu. This is very ugly. Will refactor later.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,114 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 import Head from "../components/head"
 import indexStyles from "../styles/index.module.scss"
 
-const Home = () => {
-    return (
-        <div className={indexStyles.wrapper}>
-            <Head title="Home" />
+class Home extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            isOpen: false,
+            ariaExpanded: false,
+            backgroundColor: "transparent",
+        }
 
-            <h1>Valiant Effort LLC</h1>
+        this.toggleMenu = this.toggleMenu.bind(this)
+    }
 
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/annie">Annie</Link>
-                    </li>
-                    <li>
-                        <Link to="/team">Team</Link>
-                    </li>
-                    <li>
-                        <Link to="/blog">Blog</Link>
-                    </li>
-                    <li>
-                        <Link to="/events">Events</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                    <li>
-                        <Link to="/donate">Donate</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    )
+    toggleMenu() {
+        this.setState(prevState => {
+            return {
+                isOpen: !prevState.isOpen,
+                ariaExpanded: !prevState.ariaExpanded,
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className={indexStyles.wrapper}>
+                <Head title="Home" />
+
+                <header className={indexStyles.header}>
+                    <nav
+                        style={
+                            this.state.isOpen
+                                ? { backgroundColor: "rgba(0, 0, 0, .55)" }
+                                : { backgroundColor: "transparent" }
+                        }
+                        className={indexStyles.mobileNav}
+                    >
+                        <button
+                            onClick={this.toggleMenu}
+                            className={indexStyles.toggleMenuButton}
+                        >
+                            <span className={indexStyles.srOnly}>Menu</span>
+                            <span className={indexStyles.hamburgerIcon}>
+                                <div className={indexStyles.hamburgerTop}></div>
+                                <div
+                                    className={indexStyles.hamburgerMiddle}
+                                ></div>
+                                <div
+                                    className={indexStyles.hamburgerBottom}
+                                ></div>
+                            </span>
+                        </button>
+
+                        {this.state.isOpen && (
+                            <ul
+                                id="mobile-nav-list"
+                                className={indexStyles.mobileNavList}
+                            >
+                                <li>
+                                    <Link to="/annie">Annie</Link>
+                                </li>
+                                <li>
+                                    <Link to="/team">Team</Link>
+                                </li>
+                                <li>
+                                    <Link to="/blog">Blog</Link>
+                                </li>
+                                <li>
+                                    <Link to="/events">Events</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact">Contact</Link>
+                                </li>
+                                <li>
+                                    <Link to="/donate">Donate</Link>
+                                </li>
+                            </ul>
+                        )}
+                    </nav>
+
+                    <h1 className={indexStyles.logo}>Valiant Effort LLC</h1>
+                </header>
+
+                <nav className={indexStyles.fullNav}>
+                    <ul className={indexStyles.navList}>
+                        <li>
+                            <Link to="/annie">Annie</Link>
+                        </li>
+                        <li>
+                            <Link to="/team">Team</Link>
+                        </li>
+                        <li>
+                            <Link to="/blog">Blog</Link>
+                        </li>
+                        <li>
+                            <Link to="/events">Events</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                        <li>
+                            <Link to="/donate">Donate</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
 }
 
 export default Home
