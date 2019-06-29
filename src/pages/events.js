b7b8427ff717023c00012afa1c27b84dda705b77
Tia Esguerra <msksfo@gmail.com>
2019-06-29 09:22:26 -0700

    Fix: make calendar iframe responsive

--- src/pages/events.js
+++ src/pages/events.js
@@ -10,8 +10,12 @@ const Events = () => {
             <div className={eventStyles.wrapper}>
                 <div className={eventStyles.iframeContainer}>
                     <iframe
+                        className={eventStyles.calendarIframe}
                         title="Valiant Effort Calendar Of Events"
-                        src="https://calendar.google.com/calendar/b/2/embed?wkst=1&amp;bgcolor=%23039BE5&amp;ctz=America%2FLos_Angeles&amp;src=ZWNjdDNkZHIxcHJzY2ZpMmRtYnAycmZtZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%238E24AA&amp;color=%230B8043&amp;showTitle=0&amp;showPrint=0&amp;showCalendars=0"
+                        src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23039BE5&amp;ctz=America%2FLos_Angeles&amp;src=ZWNjdDNkZHIxcHJzY2ZpMmRtYnAycmZtZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%238E24AA&amp;color=%230B8043&amp;showTitle=0&amp;showPrint=0&amp;showCalendars=0"
+                        width="800"
+                        height="600"
+                        frameborder="0"
                         scrolling="no"
                     ></iframe>
                 </div>
