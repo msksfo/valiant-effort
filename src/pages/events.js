06775ba20a43ed155232b8ac4eff8d8c5e33c18f
Tia Esguerra <msksfo@gmail.com>
2019-06-29 11:35:14 -0700

    Wip: add a separate iframe for mobile devices, showing agenda view. Because content on the right of the default view was being cut off.

--- src/pages/events.js
+++ src/pages/events.js
@@ -8,13 +8,25 @@ const Events = () => {
         <Layout>
             <Head title="Events" />
             <div className={eventStyles.wrapper}>
+                <div className={eventStyles.mobileIframeContainer}>
+                    <iframe
+                        className={eventStyles.calendarIframe}
+                        title="Valiant Effort Calendar Of Events"
+                        src="https://calendar.google.com/calendar/b/2/embed?height=300&amp;wkst=1&amp;bgcolor=%23039BE5&amp;ctz=America%2FLos_Angeles&amp;src=ZWNjdDNkZHIxcHJzY2ZpMmRtYnAycmZtZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%238E24AA&amp;color=%230B8043&amp;showCalendars=0&amp;showTz=1&amp;showTabs=0&amp;showPrint=0&amp;showTitle=0&amp;showNav=0&amp;showDate=1&amp;mode=AGENDA"
+                        width="auto"
+                        height="300"
+                        frameborder="0"
+                        scrolling="no"
+                    ></iframe>
+                </div>
+
                 <div className={eventStyles.iframeContainer}>
                     <iframe
                         className={eventStyles.calendarIframe}
                         title="Valiant Effort Calendar Of Events"
-                        src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23039BE5&amp;ctz=America%2FLos_Angeles&amp;src=ZWNjdDNkZHIxcHJzY2ZpMmRtYnAycmZtZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%238E24AA&amp;color=%230B8043&amp;showTitle=0&amp;showPrint=0&amp;showCalendars=0"
-                        width="800"
-                        height="600"
+                        src="https://calendar.google.com/calendar/b/2/embed?height=300&amp;wkst=1&amp;bgcolor=%23039BE5&amp;ctz=America%2FLos_Angeles&amp;src=ZWNjdDNkZHIxcHJzY2ZpMmRtYnAycmZtZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%238E24AA&amp;color=%230B8043&amp;showTitle=0&amp;showPrint=0&amp;showCalendars=0"
+                        width="auto"
+                        height="300"
                         frameborder="0"
                         scrolling="no"
                     ></iframe>
