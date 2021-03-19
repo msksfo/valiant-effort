02f81f6842078a26541ed469f07134690852e870
Tia Esguerra <msksfo@gmail.com>
2021-03-19 09:28:36 -0700

    Feat: add state to team member component, so the card can open/close

--- src/components/teamMember/teamMember.js
+++ src/components/teamMember/teamMember.js
@@ -1,25 +1,41 @@
-import React from "react"
+import React, {useState}  from "react"
+import cx from 'classnames'
 import teamMemberStyles from "../teamMember/teamMember.module.scss"
 
+
+
 const TeamMember = props => {
+   const [isActive, setActive] = useState(false)
+
+   const handleClick = (e) => {
+        // console.log(e.currentTarget.nextElementSibling)
+         setActive(!isActive);
+    }
+
     return (
-        <li
-            style={{ borderTop: `3px solid ${props.borderColor}` }}
-            className={`${teamMemberStyles.teamMemberLi}`}
-        >
+        <li className={teamMemberStyles.teamMemberLi}>
+            
             <img
                 className={teamMemberStyles.profilePic}
-                src={`${props.imageSource}`}
+                src={ !isActive ? `${props.cartoonImageSource}` : `${props.imageSource}`}
                 alt={props.name}
             />
 
-            <h3 className={teamMemberStyles.teamMemberName}>{props.name}</h3>
+            <button className={teamMemberStyles.btn} onClick={handleClick}>
+                <span className={teamMemberStyles.open}>&#8964;</span>
+                    <span className={teamMemberStyles.close}>&times;</span>
+            </button>
 
-            {props.quote && (
-                <q className={teamMemberStyles.quote}>{props.quote}</q>
-            )}
+            <div className={ isActive ? cx(teamMemberStyles.textContent, teamMemberStyles.active) : cx(teamMemberStyles.textContent) }>
+                <h3 className={teamMemberStyles.teamMemberName}>{props.name}</h3>
 
-            <p>{props.bio}</p>
+                {props.quote && (
+                    <q className={teamMemberStyles.quote}>{props.quote}</q>
+                )}
+
+                <p>{props.bio}</p>
+            </div>
+               
         </li>
     )
 }
